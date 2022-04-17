c002f1bc3368acbf75bc127a336d3fb2f85fa0cb
ochoapjavier <ochoapjavier@gmail.com>
2022-04-17 20:18:44 +0200

    PB 2

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -13,6 +13,7 @@ export class EmpleadoComponent implements OnInit {
   //empresa="Sensu";
   habilitacionCuadro=true;
   usuarioRegistrado=false;
+  textoDeRegistro="No hay nadie registrado";
 
   getRegistroUsuario(){
     this.usuarioRegistrado=false
@@ -21,6 +22,16 @@ export class EmpleadoComponent implements OnInit {
 
   llamaEmpresa(value:String){
     
+  }
+
+  setUsuarioRegistrado(event: Event){
+    //alert("El usuario se acaba de registrar");
+    //this.textoDeRegistro="El usuario se acaba de registrar";
+    if ((<HTMLInputElement>event.target).value=="si") {
+      this.textoDeRegistro="El usuario se acaba de registrar";
+    } else{
+      this.textoDeRegistro="No hay nadie registrado";
+    }
   }
 
   constructor() { }
